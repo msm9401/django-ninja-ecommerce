dfe5099eaff33e088b0351a2dced9554f4e338b4
msm9401 <anstmdals94@gmail.com>
2024-06-19 23:38:03 +0900

    Feat : JWT 인증

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 
+from user.authentication import bearer_auth
+
 
 base_api = NinjaAPI(title="Ecommerce", version="0.0.0")
 
@@ -9,6 +11,14 @@ base_api = NinjaAPI(title="Ecommerce", version="0.0.0")
 @base_api.get("")
 def health_check_handler(request):
     return {"ping": "pong"}
+
+
+@base_api.get("/auth-test", auth=bearer_auth)
+def auth_test(request):
+    return {
+        "token": request.auth,
+        "email": request.user.email,
+    }
 
 
 urlpatterns = [
