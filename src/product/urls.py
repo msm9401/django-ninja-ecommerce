8b18e0b9f55a254f42e973fe75df0dc1e49b087b
msm9401 <anstmdals94@gmail.com>
2024-07-09 23:51:33 +0900

    Feat : Category 리스트 조회

--- src/product/urls.py
+++ src/product/urls.py
@@ -2,8 +2,8 @@ from django.http import HttpRequest
 from ninja import Router
 
 from config.response import ObjectResponse, response
-from product.models import Product, ProductStatus
-from product.response import ProductListResponse
+from product.models import Category, Product, ProductStatus
+from product.response import CategoryListResponse, ProductListResponse
 
 
 router = Router(tags=["Products"])
@@ -21,5 +21,19 @@ def product_list_handler(request: HttpRequest):
             products=Product.objects.filter(status=ProductStatus.ACTIVE).values(
                 "id", "name", "price"
             )
+        )
+    )
+
+
+@router.get(
+    "/categories",
+    response={
+        200: ObjectResponse[CategoryListResponse],
+    },
+)
+def categories_list_handler(request: HttpRequest):
+    return 200, response(
+        CategoryListResponse.build(
+            categories=Category.objects.filter(parent=None).prefetch_related("children")
         )
     )
